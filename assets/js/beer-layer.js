a926bc78317ae2271765d33991f20a0ee2751ff7
Damian <7105046+damianbenente@users.noreply.github.com>
2021-11-07 06:04:59 -0300

    Update beer-layer.js

--- assets/js/beer-layer.js
+++ assets/js/beer-layer.js
@@ -19,7 +19,7 @@ function display_restaurant(display)
 function make_overlayAll(restaurant)
 {
     var overpass_url = "data=[out:json];(node(BBOX)[amenity=bar]['brewery'!='none'];way(BBOX)[amenity=bar]['brewery'!='none'];node(BBOX)[amenity=cafe]['cuisine'!='coffee_shop']['brewery'!='none'];way(BBOX)[amenity=cafe]['cuisine'!='coffee_shop']['brewery'!='none'];node(BBOX)[amenity=biergarten]['brewery'!='none'];node(BBOX)[microbrewery=yes]['brewery'!='none'];node(BBOX)['brewery']['brewery'!='none'];way(BBOX)['brewery']['brewery'!='none'];node(BBOX)[amenity=pub]['brewery'!='none'];way(BBOX)[amenity=pub]['brewery'!='none'];";
-    console.log("make_overlayAll   test11113");
+    console.log("make_overlayAll   test11114");
     if (typeof restaurant === 'undefined' || restaurant)
     {
         overpass_url += "node(BBOX)[amenity=restaurant]['brewery'!='none'];way(BBOX)[amenity=restaurant]['brewery'!='none'];"
@@ -91,7 +91,7 @@ function draw_beer(query, icon, surcharge)
                //console.log(data.elements.length);
                //console.log(data);
  
-         
+            console.log(data);
             for(var i = 0 ; i < data.elements.length ; i++)
             {
                 //console.log(i);
